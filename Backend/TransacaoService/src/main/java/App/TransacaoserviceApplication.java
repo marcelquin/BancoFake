eb3e3b1cd625fe67bdb83743da863634e31d7b49
Marcel Higor quinteiro Araujo <117388829+marcelquin@users.noreply.github.com>
2024-12-19 15:24:09 -0400

    adição de projetos base
    projetos base descompactados, prontos para inicio de desenvolvimento

---
+++ Backend/TransacaoService/src/main/java/App/TransacaoserviceApplication.java
@@ -0,0 +1,13 @@
+package App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransacaoserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransacaoserviceApplication.class, args);
+	}
+
+}
